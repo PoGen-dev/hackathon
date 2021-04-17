91d821f2c453142e225fb6b93d30bede3be08b67
PoGen-dev <archangelgena@mail.ru>
2021-04-18 00:02:40 +0300

    Update doc-string in Main

--- Package/Source/Main.py
+++ Package/Source/Main.py
@@ -10,56 +10,88 @@ class TelegramBot:
 #   COMMANDS
 
     @dispatcher.message_handler(commands = ['start'])
-    async def StartCommand(Message: aiogram.types.Message):
+    async def StartCommand(Message: aiogram.types.Message) -> None:
         """
+        Reaction to command '/start'.
+
+        :param aiogram.types.Message Message: 
+        :return NoneType:
         """
         await Activity.StartMessage(Message)
 
     @dispatcher.message_handler(commands = ['help'])
-    async def HelpCommand(Message: aiogram.types.Message):
+    async def HelpCommand(Message: aiogram.types.Message) -> None:
         """
+        Reaction to command '/help'.
+
+        :param aiogram.types.Message Message: 
+        :return NoneType:
         """
         await Activity.HelpMessage(Message)
     
     @dispatcher.message_handler(commands = ['status'])
-    async def StatusCommand(Message: aiogram.types.Message):
+    async def StatusCommand(Message: aiogram.types.Message) -> None:
         """
+        Reaction to command '/status'.
+
+        :param aiogram.types.Message Message: 
+        :return NoneType:
         """
         await Activity.StatusMessage(Message)
 
     @dispatcher.message_handler(commands = ['rule'])
-    async def RuleCommand(Message: aiogram.types.Message):
+    async def RuleCommand(Message: aiogram.types.Message) -> None:
         """
+        Reaction to command '/rule'.
+
+        :param aiogram.types.Message Message: 
+        :return NoneType:
         """
         await Activity.RuleMessage(Message)
 
 #   CONTENT TYPES
 
     @dispatcher.message_handler(content_types=['text'])
-    async def message_reply(Message: aiogram.types.Message):
+    async def message_reply(Message: aiogram.types.Message) -> None:
         """
+        Reaction to text written by user.
+
+        :param aiogram.types.Message Message:
+        :return None:
         """
         await Activity.ReplyMessage(Message)
 
     @dispatcher.message_handler(content_types = ['document'])
-    async def take_file(Message: aiogram.types.Message):
+    async def take_file(Message: aiogram.types.Message) -> None:
         """
+        Reaction to document sent by user.
+
+        :param aiogram.types.Message Message:
+        :return None:
         """
         await Activity.GetDocument(Message)
 
 #   STATE
 
     @dispatcher.message_handler(state = RegistrationState.DocIndex)
-    async def EnterDocIndex(Message: aiogram.types.Message, state: aiogram.dispatcher.storage.FSMContext):
+    async def EnterDocIndex(Message: aiogram.types.Message, state: aiogram.dispatcher.storage.FSMContext) -> None:
         """
+        Reaction to text written by user in DocIndex state.
+
+        :param aiogram.types.Message Message:
+        :return None:
         """
         await Activity.ReactionOnState(Message, state)
 
 #   CALLBACK QUERY
 
     @dispatcher.callback_query_handler(lambda Message: True)
-    async def ProcessCallbackQuery(CallbackQuery: aiogram.types.callback_query.CallbackQuery):
+    async def ProcessCallbackQuery(CallbackQuery: aiogram.types.callback_query.CallbackQuery) -> None:
         """
+        Reaction to using InlineKeyboardMarkup by user.
+
+        :param aiogram.types.Message Message:
+        :return None:
         """
         Code = int(CallbackQuery.data)
         ActivityDict = {
